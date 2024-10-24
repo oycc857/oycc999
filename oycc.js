9c022c0af920d600b2b72c2d6fb758bac09ebe37
oycc857 <3080690467@qq.com>
2024-10-24 23:35:19 +0800

    Add files via upload

--- oycc.js
+++ oycc.js
@@ -1,3 +1,6 @@
+alert('请使用edge浏览器打开此网站,js能获取摄像头和麦克风权限,但是不能获取自动播放媒体权限,可以按照下面流程打开,然后进入网站'
+    
+)
 var daohang = document.querySelector('.daohang');
 var lis = daohang.querySelectorAll('li');
 var items = document.querySelectorAll('.item');
