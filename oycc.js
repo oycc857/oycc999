22e43c04fcf44d1f84ff29d30feadd433f0590c1
oycc857 <3080690467@qq.com>
2024-10-28 22:47:33 +0800

    Add files via upload

--- oycc.js
+++ oycc.js
@@ -16,3 +16,25 @@ for (var i = 0; i < lis.length; i++) {
         items[index].style.display = 'block';
     }
 }
+$(".no1").hover(function(){
+    $(this).children("ul").stop().slideToggle(200);
+})
+
+$(".no2").hover(function () {
+    $(this).children("ul").stop().slideToggle(200);
+})
+$(".no3").hover(function () {
+    $(this).children("ul").stop().slideToggle(200);
+})
+$(".no4").hover(function () {
+    $(this).children("ul").stop().slideToggle(200);
+})
+$(".no5").hover(function () {
+    $(this).children("ul").stop().slideToggle(200);
+})
+$(".no6").hover(function () {
+    $(this).children("ul").stop().slideToggle(200);
+})
+$(".no7").hover(function () {
+    $(this).children("ul").stop().slideToggle(200);
+})
